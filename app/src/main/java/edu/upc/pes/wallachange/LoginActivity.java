bb45e63ab44d609e4d5b0065fa4ea9f3fb6d748e
JordiFructos <jordifructos@gmail.com>
2017-03-24 19:45:13 +0100

    añadido main activity, enlace entre main y login, empezado navigation v2
    añadido main activity, enlace entre main y login, empezado navigation v2

--- app/src/main/java/edu/upc/pes/wallachange/LoginActivity.java
+++ app/src/main/java/edu/upc/pes/wallachange/LoginActivity.java
@@ -1,6 +1,8 @@
 package edu.upc.pes.wallachange;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -9,9 +11,13 @@ import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterAuthToken;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterAuthClient;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import java.util.Locale;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -21,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TWITTER_SECRET = "ea81fYjYJsa2RcqlA5H3QmZ9RKxoQZtgmg0bNdFlzVVanBOj4Q";
 
     private TwitterLoginButton loginButton;
+    private String lenguage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,23 @@ public class LoginActivity extends AppCompatActivity {
         Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_login);
 
+        SharedPreferences preferences = this.getPreferences(Context.MODE_PRIVATE);
+        final String prefLenguage = preferences.getString("language","empty");
+        if (prefLenguage.equals("empty")) {
+            String lenguage = Locale.getDefault().getDisplayLanguage();
+            SharedPreferences.Editor editor = preferences.edit();
+            if (lenguage.equals("Español")) {
+                lenguage = getString(R.string.lenguage_esp);
+                editor.putString("language",lenguage);
+            }
+            else  {
+                lenguage = getString(R.string.lenguage_eng);
+                editor.putString("language",lenguage);
+            }
+            editor.apply();
+        }
+        else lenguage = prefLenguage;
+
         loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
         loginButton.setCallback(new Callback<TwitterSession>() {
             @Override
@@ -36,12 +60,31 @@ public class LoginActivity extends AppCompatActivity {
                 // The TwitterSession is also available through:
                 // Twitter.getInstance().core.getSessionManager().getActiveSession()
                 TwitterSession session = result.data;
-                // TODO: Remove toast and use the TwitterSession's userID
-                // with your app's user model
+                String name = session.getUserName();
+                TwitterAuthToken token = session.getAuthToken();
+                /**
+                TwitterAuthClient authClient = new TwitterAuthClient();
+                authClient.requestEmail(session, new Callback<String>() {
+                    @Override
+                    public void success(Result<String> result) {
+                        String email = result.data;
+                        Toast.makeText(getApplicationContext(),email, Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void failure(TwitterException exception) {
+                        Toast.makeText(getApplicationContext(), "Email not authorited", Toast.LENGTH_LONG).show();
+                    }
+                });
+                **/
+                //TODO: Database
+                login(name);
             }
             @Override
             public void failure(TwitterException exception) {
-                Toast.makeText(getApplicationContext(), "Login with Twitter failure", Toast.LENGTH_LONG).show();
+                //TODO: Get preferences
+                if (lenguage.equals(getString(R.string.lenguage_eng))) Toast.makeText(getApplicationContext(),R.string.errorIncorrectLogin_eng, Toast.LENGTH_LONG).show();
+                else Toast.makeText(getApplicationContext(),R.string.errorIncorrectLogin_esp, Toast.LENGTH_LONG).show();
             }
         });
 
@@ -55,4 +98,12 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.onActivityResult(requestCode, resultCode, data);
     }
 
+    private void login (String name) {
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.putExtra("user",name);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/edu/upc/pes/wallachange/MainActivity.java
+++ app/src/main/java/edu/upc/pes/wallachange/MainActivity.java
@@ -1,23 +1,55 @@
 package edu.upc.pes.wallachange;
 
+import android.app.FragmentManager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import io.fabric.sdk.android.Fabric;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static final String TWITTER_KEY = "QFTeG0RyQsc57qVkFh9tTUjj0";
     private static final String TWITTER_SECRET = "dHoLLt2b4yDA9iWbXQdfyh7c5d3nYU2PVQNlNziQ7L0BijETkW";
 
+    private HomeFragment homeFragment;
+
+    private FragmentManager myFragmentManager;
+    private DrawerLayout myDrawer;
+    private String user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
-        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            user = extras.getString("user");
+        }
+        //TODO:lenguage
+        NavigationView myNavigation = (NavigationView) findViewById(R.id.navigationView);
+        myNavigation.setNavigationItemSelectedListener(this);
+
+        myDrawer = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        myFragmentManager = getFragmentManager();
+
+        homeFragment = new HomeFragment();
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.navigationMain;
+        }
     }
 }
