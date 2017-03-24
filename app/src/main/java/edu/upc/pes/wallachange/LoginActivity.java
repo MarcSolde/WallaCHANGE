fd73dbacf6c18ac9f0d3479e0d16129251ca758b
sejoDjoseon <sergio.morales93@gmail.com>
2017-03-24 19:37:21 +0100

    prova login tw i fb correcte

--- app/src/main/java/edu/upc/pes/wallachange/LoginActivity.java
+++ app/src/main/java/edu/upc/pes/wallachange/LoginActivity.java
@@ -1,10 +1,17 @@
 package edu.upc.pes.wallachange;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
@@ -20,7 +27,10 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TWITTER_KEY = "6YPYzTVYffWUgukyKkaNry1Lg";
     private static final String TWITTER_SECRET = "ea81fYjYJsa2RcqlA5H3QmZ9RKxoQZtgmg0bNdFlzVVanBOj4Q";
 
-    private TwitterLoginButton loginButton;
+    private TextView loginInfo;
+    private TwitterLoginButton twLoginButton;
+    private LoginButton fbLoginButton;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +39,8 @@ public class LoginActivity extends AppCompatActivity {
         Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_login);
 
-        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
-        loginButton.setCallback(new Callback<TwitterSession>() {
+        twLoginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+        twLoginButton.setCallback(new Callback<TwitterSession>() {
             @Override
             public void success(Result<TwitterSession> result) {
                 // The TwitterSession is also available through:
@@ -38,10 +48,46 @@ public class LoginActivity extends AppCompatActivity {
                 TwitterSession session = result.data;
                 // TODO: Remove toast and use the TwitterSession's userID
                 // with your app's user model
+                loginInfo.setText("Login twitter succesful.");
             }
             @Override
             public void failure(TwitterException exception) {
                 Toast.makeText(getApplicationContext(), "Login with Twitter failure", Toast.LENGTH_LONG).show();
+                loginInfo.setText("Login attempt failed.");
+            }
+        });
+
+        loginInfo = (TextView)findViewById(R.id.loginInfo);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
+
+        fbLoginButton = (LoginButton) findViewById(R.id.fb_login_button);
+        fbLoginButton.setReadPermissions("email");
+        // If using in a fragment
+        //loginButton.setFragment(this);
+
+        // Callback registration
+        fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                loginInfo.setText(
+                        "User ID: "
+                                + loginResult.getAccessToken().getUserId()
+                                + "\n" +
+                                "Auth Token: "
+                                + loginResult.getAccessToken().getToken()
+                );
+            }
+
+            @Override
+            public void onCancel() {
+                loginInfo.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                loginInfo.setText("Login attempt failed.");
             }
         });
 
@@ -52,7 +98,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         // Make sure that the loginButton hears the result from any
         // Activity that it triggered.
-        loginButton.onActivityResult(requestCode, resultCode, data);
+        twLoginButton.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
 }
