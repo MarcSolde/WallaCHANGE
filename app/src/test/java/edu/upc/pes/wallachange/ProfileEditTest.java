07cb4a0de4ca7d91240000d1711f41482257086c
Carlota Prat <carlota.prat@est.fib.upc.edu>
2017-04-06 20:39:23 +0200

    editProfile amb alguna modificació

---
+++ app/src/test/java/edu/upc/pes/wallachange/ProfileEditTest.java
@@ -0,0 +1,23 @@
+package edu.upc.pes.wallachange;
+
+import android.content.Context;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Created by carlota on 6/4/17.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ProfileEditTest {
+
+    @Mock
+    Context myContext;
+
+    @Test
+    public void location_isNotEmpty() throws Exception {
+//        https://elandroidelibre.elespanol.com/2016/03/como-testear-tu-aplicacion-en-android.html
+    }
+}
