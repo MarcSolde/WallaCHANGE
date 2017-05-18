02c6441ab43e569c092ac33f2b7af9970fa2db20
sejoDjoseon <sergio.morales93@gmail.com>
2017-05-18 23:37:57 +0200

    undefined

--- app/src/main/java/edu/upc/pes/wallachange/LoginSystem/CallbackFacebook.java
+++ app/src/main/java/edu/upc/pes/wallachange/LoginSystem/CallbackFacebook.java
@@ -63,14 +63,14 @@ public class CallbackFacebook implements FacebookCallback<LoginResult> {
 
         headers.put("Content-Type", "application/json");
 
-        adapter.POSTRequestAPI("http://10.0.2.2:3000/loginFB",
+        //adapter.POSTSJsonObjectRequestAPI("http://10.0.2.2:3000/loginFB",
+        adapter.POSTSJsonObjectRequestAPI("http://104.236.98.100:3000/loginFB",
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        JSONObject js = response;
                         try {
                             CurrentUser user = CurrentUser.getInstance();
-                            user.setToken(js.getString("token"));
+                            user.setToken(response.getString("token"));
 //                            user.setUsername(js.getString("nom"));
 //                            user.setLocation(js.getString("localitat"));
 //                            user.setPreferencesArray(js.getJSONArray("prefs"));
@@ -128,7 +128,9 @@ public class CallbackFacebook implements FacebookCallback<LoginResult> {
             params.put("token", token);
             params.put("id", id);
             headers.put("Content-Type", "application/json");
-            adapter.POSTRequestAPI("http://10.0.2.2:3000/loginFB",
+
+            //adapter.POSTSJsonObjectRequestAPI("http://10.0.2.2:3000/loginFB",
+            adapter.POSTSJsonObjectRequestAPI("http://104.236.98.100:3000/loginFB",
                     new Response.Listener<JSONObject>() {
                         @Override
                         public void onResponse(JSONObject response) {
