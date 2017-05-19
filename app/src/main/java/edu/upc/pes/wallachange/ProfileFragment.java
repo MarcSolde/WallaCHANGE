c7493c517554f6c509f37f520a9fc3d390e4b47f
JordiFructos <jordifructos@gmail.com>
2017-05-19 21:15:52 +0200

    modificacions profile de carlota

--- app/src/main/java/edu/upc/pes/wallachange/ProfileFragment.java
+++ app/src/main/java/edu/upc/pes/wallachange/ProfileFragment.java
@@ -86,7 +86,7 @@ public class ProfileFragment extends Fragment  implements View.OnClickListener {
         mRatingBar = (RatingBar) view.findViewById(R.id.ratingBar);
         submitProfile = (Button) view.findViewById(R.id.submitButton);
         editTextPref = (EditText) view.findViewById(R.id.addPreference);
-
+        usernameField.setText(username);
 
 
         fotoPerfil.setImageURI(null);
@@ -153,17 +153,7 @@ public class ProfileFragment extends Fragment  implements View.OnClickListener {
 
             case R.id.submitButton:
                 location = locationTE.getText().toString();
-                if (location.trim().length() != 0) {
-                    if (!user.getLocation().isEmpty()) {
-                        if (!user.getLocation().equals(location)) user.setLocation(location);
-                    } else {
-                        user.setLocation(location);
-                    }
 
-                } else {
-                    String errorEmpty = getResources().getString(R.string.errorEmptyField_eng);
-                    locationTE.setError(errorEmpty);
-                }
                 user.setPreferencesArrayList(prefs);String token = user.getToken();
                 String location = user.getLocation();
                 ArrayList<String> prefs = user.getPreferences();
@@ -185,7 +175,7 @@ public class ProfileFragment extends Fragment  implements View.OnClickListener {
                 Map<String,String> headers = new HashMap<>();
                 headers.put("Content-Type", "application/json");
                 AdapterAPIRequest adapter = new AdapterAPIRequest();
-                adapter.PUTRequestAPI("http://10.0.2.2:3000/updateUser/"+user.getUsername(),
+                adapter.PUTRequestAPI("http://104.236.98.100:3000/updateUser/"+user.getId(),
                         new Response.Listener<JSONObject>() {
                             @Override
                             public void onResponse(JSONObject response) {
