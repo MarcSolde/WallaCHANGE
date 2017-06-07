12ebe9b9361a486a92c7fcdcbf3dec021f56ae1f
JordiFructos <jordifructos@gmail.com>
2017-06-07 14:49:07 +0200

    ya se peuden consultar tus propios elementos

---
+++ app/src/main/java/edu/upc/pes/wallachange/YourItemsFragment.java
@@ -0,0 +1,30 @@
+package edu.upc.pes.wallachange;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.upc.pes.wallachange.debug.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class YourItemsFragment extends Fragment {
+
+
+    public YourItemsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_your_items, container, false);
+    }
+
+}
