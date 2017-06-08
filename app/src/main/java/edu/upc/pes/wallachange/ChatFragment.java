abc9c8a185741535b7bd52d372f76a153bf22ebb
Carlota Prat <carlota.prat@est.fib.upc.edu>
2017-06-09 00:45:56 +0200

    mes chat..

--- app/src/main/java/edu/upc/pes/wallachange/ChatFragment.java
+++ app/src/main/java/edu/upc/pes/wallachange/ChatFragment.java
@@ -2,18 +2,15 @@ package edu.upc.pes.wallachange;
 
 import static com.android.volley.VolleyLog.TAG;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Fragment;
-import android.app.FragmentManager;
 import android.content.DialogInterface;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
-import android.support.design.widget.NavigationView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -23,33 +20,33 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 import edu.upc.pes.wallachange.APILayer.AdapterAPIRequest;
 import edu.upc.pes.wallachange.Adapters.ChatAdapter;
-import edu.upc.pes.wallachange.Adapters.PreferencesAdapter;
-import edu.upc.pes.wallachange.Adapters.UserListAdapter;
 import edu.upc.pes.wallachange.Models.Conversa;
 import edu.upc.pes.wallachange.Models.CurrentUser;
 import edu.upc.pes.wallachange.Models.Message;
 import edu.upc.pes.wallachange.Models.User;
+
 
 /**
  * Created by carlota on 3/6/17.
@@ -79,7 +76,28 @@ public class ChatFragment extends Fragment implements View.OnClickListener {
     private User otherUser;
     private Boolean confirmat;
     private Boolean cancelat;
-    View view;
+    private View view;
+    private com.github.nkzawa.socketio.client.Socket mSocket;
+    {
+        try {
+            mSocket = IO.socket("http://10.0.3.2:3002/");
+        } catch (URISyntaxException e) {}
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mSocket.on("msg", onNewMessage);
+        mSocket.connect();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mSocket.disconnect();
+        mSocket.off("msg", onNewMessage);
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -114,11 +132,63 @@ public class ChatFragment extends Fragment implements View.OnClickListener {
         buttonSendChat.setOnClickListener(this);
 
         conv_id = getArguments().getString("conversa");
+        Log.i("IDDDDDD", conv_id);
         getParamsConversa();
         getConversa();
 
         return view;
     }
+
+
+
+    private void attemptSend() {
+        String message = editTextChat.getText().toString().trim();
+        if (TextUtils.isEmpty(message)) {
+            return;
+        }
+
+        editTextChat.setText("");
+        JSONObject js = new JSONObject();
+        try {
+            js.put("room", conv_id);
+            js.put("msg", message);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Message mes = new Message();
+        mes.setMessage(message);
+        mes.setOwner(currentUser.getId());
+        messages.add(mes);
+        loadMessages(messages);
+        mSocket.emit("msg", js);
+    }
+
+
+    private Emitter.Listener onNewMessage = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    JSONObject data = (JSONObject) args[0];
+                    String username;
+                    String message;
+                    try {
+                        username = data.getString("author");
+                        message = data.getString("msg");
+                    } catch (JSONException e) {
+                        return;
+                    }
+
+                    Message m = new Message();
+                    m.setMessage(message);
+                    m.setOwner(username);
+                    messages.add(m);
+                    loadMessages(messages);
+                }
+            });
+        }
+    };
 
 
     @Override
@@ -127,16 +197,11 @@ public class ChatFragment extends Fragment implements View.OnClickListener {
             case R.id.editTextChat:
                 break;
             case R.id.buttonSendChat:
-                String textMessage = editTextChat.getText().toString();
-                if (!textMessage.equals("")) {
-                    Message mes6 = new Message(textMessage, currentUser.getId());
-                    sendMissage(mes6);
-                    editTextChat.setText("");
-                }
-                else {
-                    String errorEmpty = getResources().getString(R.string.errorEmptyField_eng);
-                    editTextChat.setError(errorEmpty);
-                }
+                attemptSend();
+//                else {
+//                    String errorEmpty = getResources().getString(R.string.errorEmptyField_eng);
+//                    editTextChat.setError(errorEmpty);
+//                }
                 break;
         }
     }
@@ -144,11 +209,6 @@ public class ChatFragment extends Fragment implements View.OnClickListener {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.menu_fragment_chat, menu);
-//            MenuItem item = menu.getItem(0);
-//            MenuItem item2 = menu.getItem(2);
-//            item.setVisible(false);
-//            item2.setVisible(false);
-//            myActivity.invalidateOptionsMenu();
 
     }
 
@@ -424,80 +484,5 @@ public class ChatFragment extends Fragment implements View.OnClickListener {
 
 
 
+
 }
-
-//    public void getAlterUser(String alterUserId) {
-//        Log.i("crida obtenirUser", alterUserId);
-//        Map<String, String> headers1 = new HashMap<>();
-//
-//        adapterAPI = new AdapterAPIRequest();
-//        final User u = new User();
-//        adapterAPI.GETRequestAPI("/user/"+alterUserId,
-//                new Response.Listener<JSONObject>() {
-//
-//                    @Override
-//                    public void onResponse(JSONObject response) {
-//                        try {
-//                            Log.i("JSONbooo ",response.toString());
-//                            u.setUsername(response.getString("nom"));
-//                            u.setId(response.getString("id"));
-//                            setTitle(u.getUsername());
-//                        }
-//                        catch (JSONException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                },
-//                new Response.ErrorListener() {
-//
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        Log.i("JSONerror: ","");
-//                    }
-//                },
-//                headers1
-//        );
-//
-//    }
-
-//    public String getTime() {
-//        c = Calendar.getInstance();
-//        String time;
-//        int hour = c.get(Calendar.HOUR_OF_DAY);
-//        int min = c.get(Calendar.MINUTE);
-//        if (min < 10) {
-//            time = hour + ".0" + min;
-//        }
-//        else time = hour + "." + min;
-//        return time;
-//    }
-
-//    public void getConverses(String token, String id) {
-//        Map<String, String> headers = new HashMap<>();
-//        headers.put("x-access-token", token);
-//        adapterAPI.GETJsonArrayRequestAPI("/chat/conversations/" + id,
-//                new Response.Listener<JSONArray>() {
-//                    @Override
-//                    public void onResponse(JSONArray response) {
-//                        try {
-//                            Log.i("resposta: ",response.toString());
-//                            ArrayList<Conversa> aux = new ArrayList<> ();
-//                            for (int i = 0;i < response.length();++i) {
-//                                JSONObject var = response.getJSONObject(i);
-//                                currentUser.addConversa(var.getString("user_id"), "asf");
-//                            }
-//                        }
-//                        catch (JSONException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                },
-//                new Response.ErrorListener() {
-//
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        Log.i("JSONerror: ","");
-//                    }
-//                }, headers
-//        );
-//    }
